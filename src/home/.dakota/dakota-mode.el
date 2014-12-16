740e0e8daef84e230be316201ced8a899fddd946
Robert Nielsen <robert@dakota.org>
2014-12-16 14:06:44 -0800

    Updated dakota-mode.el. Lots more to do.

--- src/home/.dakota/dakota-mode.el
+++ src/home/.dakota/dakota-mode.el
@@ -1,49 +1,27 @@
 ; -*- mode: Emacs-Lisp -*-
 
-(setq c-basic-offset 2)
-(setq tab-width 2)
-(setq indent-tabs-mode nil)
-
-(global-font-lock-mode t)
-
-(setq c++-font-lock-extra-types '("\\<\\(\\w+::?\\)*\\w+-t\\>"
-				  "\\<\\(\\w+::?\\)*\\w+_t\\>"))
-
-(font-lock-add-keywords 'c++-mode '(("\\<\\(make\\|dk::?\\w+\\)\\>"
-				     . font-lock-builtin-face)))
-(font-lock-add-keywords 'c++-mode '(("\\<\\(self\\|klass\\|super\\|null\\)\\>"
-				     . font-lock-keyword-face)))
-(font-lock-add-keywords 'c++-mode '(("\\<\\(finally\\)\\>"
-				     . font-lock-keyword-face)))
-(font-lock-add-keywords 'c++-mode '(("\\<\\(__import\\|__export\\|__noexport\\)\\>"
-				     . font-lock-keyword-face)))
-(font-lock-add-keywords 'c++-mode '(("\\<\\(__generic\\|__method\\)\\>"
-				     . font-lock-keyword-face)))
-(font-lock-add-keywords 'c++-mode '(("\\<\\(__selector\\|__alias\\)\\>"
-				     . font-lock-function-name-face)))
-(font-lock-add-keywords 'c++-mode '(("\\<\\(__klass\\|__trait\\)\\>"
-				     . font-lock-keyword-face)))
-(font-lock-add-keywords 'c++-mode '(("\\<\\(__superklass\\|__klass\\|__trait\\|__require\\|__provide\\|__slots\\)\\>"
-				     . font-lock-keyword-face)))
-
-(defconst c-Dakota-extra-toplevel-key "\\(namespace\\|__klass\\|__trait\\)")
+; how can we do this inside the (define-derived-mode dakota-mode ...)?
+(setq c++-font-lock-extra-types 
+      '("\\<\\(\\w+::?\\)*\\w+-t\\>"
+        "\\<\\(\\w+::?\\)*\\w+_t\\>"))
 
 (define-derived-mode dakota-mode c++-mode "Dakota"
   "Major mode for editing Dakota source files."
-
-  (setq c-extra-toplevel-key c-Dakota-extra-toplevel-key)
   (modify-syntax-entry ?- "w")
-
-  (c-set-offset 'label '+)
-  (c-set-offset 'block-open 0)
-  (c-set-offset 'substatement-open 0)
-  (c-set-offset 'case-label '+)
-  (c-set-offset 'statement-case-open 0)
+  (run-hooks 'dakota-mode-hook)
 )
-
 (provide 'dakota-mode)
 
-(add-hook 'dakota-mode-hook #'dakota-mode-font-lock)
-
-(setq auto-mode-alist (cons '("\.cc$" . c++-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\.dk$" . dakota-mode) auto-mode-alist))
+
+(defun dakota-font-lock-setup ()
+  ""
+  (font-lock-add-keywords 'dakota-mode
+                          '(("\\<\\(module\\|klass\\|trait\\)\\>" . font-lock-builtin-face)))
+
+  (font-lock-add-keywords 'dakota-mode
+                          '(("\\<\\(make\\|dk::?\\w+\\)\\>" . font-lock-builtin-face)))
+  (setq font-lock-maximum-decoration t)
+  (font-lock-mode 3)
+)
+(add-hook 'dakota-mode-hook 'dakota-font-lock-setup)
