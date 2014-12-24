d318bd0f72a45427551f4dd11e8af48e23b65eb1
Robert Nielsen <robert@dakota.org>
2014-12-23 22:59:16 -0500

    Fixed dakota-fixup-stderr.pl.

--- bin/dakota-fixup-stderr.pl
+++ bin/dakota-fixup-stderr.pl
@@ -21,8 +21,8 @@ BEGIN {
 
 use dakota::util;
 
-my $gbl_cwd = $ARGV[0] ||= '\*\*NO-CWD\*\*';
-my $gbl_parent = $ARGV[1] ||= '\*\*NO-PARENT\*\*';
+my $gbl_cwd = $ARGV[0] ||= undef;
+my $gbl_parent = $ARGV[1] ||= undef;
 
 my $name_re = qr{[\w.=+-]+};
 my $rel_dir_re = qr{$name_re/+};
@@ -30,12 +30,10 @@ my $path_re = qr{/*$rel_dir_re*?$name_re/*};
 
 sub fixup {
   my ($cwd, $parent, $file, $line) = @_;
-  $cwd = &dakota::util::canon_path($cwd);
-  $parent = &dakota::util::canon_path($parent);
   $cwd =~ s|/+$parent$||;
+  my $path = "$parent/$file";
 
-  if (-e "$cwd/$parent/$file") {
-    my $path = "$parent/$file";
+  if (-e "$cwd/$path") {
     $path = &dakota::util::canon_path($path);
     return "$path:$line:";
   } else {
@@ -46,6 +44,8 @@ sub fixup {
 while (<STDIN>) {
   my $line = $_;
   $line =~ s|^\s*[Ii]n file included from.+?:\d+:\s*$||;
-  $line =~ s|($path_re):(\d+):|&fixup($gbl_cwd, $gbl_parent, $1, $2)|eg;
+  if ($gbl_cwd && $gbl_parent) {
+    $line =~ s|($path_re):(\d+):|&fixup($gbl_cwd, $gbl_parent, $1, $2)|eg;
+  }
   print $line;
 }
