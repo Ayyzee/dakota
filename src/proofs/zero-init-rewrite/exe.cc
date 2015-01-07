f929a62d18db1d43b66aa457feacc050912ebf00
Robert Nielsen <robert@dakota.org>
2015-01-06 16:30:04 -0800

    Just added some thoughts regarding zero-init rewritting.

--- src/proofs/zero-init-rewrite/exe.cc
+++ src/proofs/zero-init-rewrite/exe.cc
@@ -1,19 +1,32 @@
-#include <stdio.h>
-#include <assert.h>
-
 #define cast(t) (t)
 
 int main()
 {
-//int foo1 = zero;
+//int smpl = zero;
 //=>
-  int foo1 = cast(decltype(foo1))0;
+  int smpl = cast(decltype(smpl))0;
 
-//struct { int x; int y; } foo2 = zero;
+//struct { int x; int y; } agg = zero;
 //=>
-//struct { int x; int y; } foo2 = { zero, zero };
+//struct { int x; int y; } agg = { zero, zero };
 //=>
-  struct { int x; int y; } foo2 = { cast(decltype(foo2.x))0, cast(decltype(foo2.x))0 };
+  struct { int x; int y; } agg = { cast(decltype(agg.x))0, cast(decltype(agg.x))0 };
 
+  /*
+
+    ?type ?ident = zero ;
+      or
+    { ?type ?ident ; ?type ?ident ; } ?ident = zero ;
+      or
+    { ?type ?ident ; ?type ?ident ; } ?ident = { zero , zero } ;
+
+    The number of zero elements in the RHS of the aggregate might be less
+    than the number of members in the LHS.
+
+    The user may want to use 'zero' for just some of the aggregate members, so its
+    not an all or nothing situation.
+
+  */
+  
   return 0;
 }
