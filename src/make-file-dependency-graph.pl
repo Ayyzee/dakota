7df5bc749f724a5a1cfe3c9991a965506038857c
Robert Nielsen <robert@dakota.org>
2015-01-19 19:53:31 -0800

    More cleanup of make-file-dependency-graph.pl.

--- src/make-file-dependency-graph.pl
+++ src/make-file-dependency-graph.pl
@@ -22,9 +22,10 @@ my $dk_files = [split("\n", `../bin/dakota-project srcs --repository $project_fi
 my $input_files = [ @$so_files, @$dk_files ];
 
 my $nrt_rep_files = {};
-my $o_files = [];
+my $o_files = {};
 my $edges = [];
 my $nrt_src_files = {};
+my $dk_cxx_files = {};
 
 my $bname = $name; $bname =~ s/\.$SO_EXT$//;
 my $rt_rep_file = "obj/rt/lib$bname.rep";
@@ -33,10 +34,8 @@ my $rt_cxx_file = "obj/rt/lib$bname.cc";
 my $rt_o_file =   "obj/rt/lib$bname.o";
 my $rt_so_file =  "obj/rt/lib$bname.$SO_EXT";
 
-my $rt_nodes = [ $rt_rep_file,
-                 $rt_hxx_file,
-                 $rt_cxx_file,
-                 $rt_o_file ];
+my $rt_files = { $rt_hxx_file => 1,
+                 $rt_cxx_file => 1 };
 
 foreach my $so_file (@$so_files) {
   my $ctlg_file = "$so_file.ctlg";
@@ -56,12 +55,14 @@ foreach my $dk_file (@$dk_files) {
   my $nrt_cxx_file = "obj/nrt/$bfile.cc";
   my $nrt_o_file =   "obj/nrt/$bfile.o";
 
+  $$dk_cxx_files{$dk_cxx_file} = 1;
+
   $$nrt_src_files{$dk_cxx_file} = 1;
   $$nrt_src_files{$nrt_hxx_file} = 1;
   $$nrt_src_files{$nrt_cxx_file} = 1;
-  
+
   $$nrt_rep_files{$nrt_rep_file} = 1;
-  push @$o_files, $nrt_o_file;
+  $$o_files{$nrt_o_file} = 1;
 
   push @$edges, [ $nrt_rep_file, $dk_file ];
   push @$edges, [ $dk_cxx_file,  $dk_file ];
@@ -81,7 +82,7 @@ push @$edges, [ $rt_cxx_file, $rt_rep_file ];
 push @$edges, [ $rt_o_file,   $rt_hxx_file ];
 push @$edges, [ $rt_o_file,   $rt_cxx_file ];
 
-foreach my $o_file (@$o_files) {
+foreach my $o_file (sort keys %$o_files) {
   push @$edges, [ $rt_so_file, $o_file ];
 }
 push @$edges, [ $rt_so_file, $rt_o_file ];
@@ -92,13 +93,22 @@ foreach my $nrt_rep_file (sort keys %$nrt_rep_files) {
 print
   "digraph {\n" .
   "  graph [ rankdir = RL, center = true, page = \"8.5,11\", size = \"7.5,10\" ];\n" .
+  "  edge  [ ];\n" .
   "  node  [ shape = rect, style = rounded, height = 0.25 ];\n" .
   "\n";
-foreach my $rt_node (@$rt_nodes) {
-  print "  \"$rt_node\" [ color = blue ];\n";
+foreach my $so_file (sort @$so_files) {
+  print "  \"$so_file\" [ color = green ];\n";
 }
 print "\n";
-foreach my $edge (@$edges) {
+foreach my $rt_file (sort keys %$rt_files) {
+  print "  \"$rt_file\" [ color = blue ];\n";
+}
+print "\n";
+foreach my $dk_cxx_file (sort keys %$dk_cxx_files) {
+  print "  \"$dk_cxx_file\" [ color = orange ];\n";
+}
+print "\n";
+foreach my $edge (sort @$edges) {
   print "  \"$$edge[0]\" -> \"$$edge[1]\"";
   if ($$nrt_src_files{$$edge[0]} && $rt_rep_file eq $$edge[1]) {
     print " [ style = dotted ]";
@@ -107,7 +117,7 @@ foreach my $edge (@$edges) {
 }
 if (1) {
   print "  { rank = same";
-  foreach my $input_file (@$input_files) {
+  foreach my $input_file (sort @$input_files) {
     print "; \"$input_file\"";
   }
   print "; }\n";
