508f13906289585f57b92979d569ce29c5f69dd1
Robert Nielsen <robert@dakota.org>
2015-02-10 16:34:47 -0800

    More cleanup of macro-system-test.sh

--- src/macro-system-test.sh
+++ src/macro-system-test.sh
@@ -8,17 +8,20 @@ else
     paths="$@"    
 fi
 
-DK_MACROS_PATH=macro-system-test-input.pl DK_PREFIX=.. ../lib/dakota/macro_system.pm $paths
+DK_MACROS_PATH=macro-system-test-input.pl DK_PREFIX=.. ../lib/dakota/macro_system.pm $paths > /tmp/summary-$$.txt 2>&1
 
 for path in $paths; do
     name=$(basename $path)
 
-    if ! diff $path macro-system-test-output/$name.cc; then
-        /bin/echo "---"
+    if ! diff $path macro-system-test-output/$name.cc > /dev/null; then
         echo "diff $path macro-system-test-output/$name.cc"
         diff $path macro-system-test-output/$name.cc || true
         echo "wc -l $path macro-system-test-output/$name.cc"
         wc -l $path
         wc -l macro-system-test-output/$name.cc
+        /bin/echo "***"
     fi
 done
+
+cat /tmp/summary-$$.txt
+rm /tmp/summary-$$.txt
