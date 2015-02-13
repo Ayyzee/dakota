100063d039d8b51d49131ea4c5a964aa5089cb01
Robert Nielsen <robert@dakota.org>
2015-02-12 23:11:05 -0800

    Cleaned up test/generate-phony-targets.sh.

--- test/generate-phony-targets.sh
+++ test/generate-phony-targets.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+set -o nounset -o errexit -o pipefail
+
 echo $@:
 
-exe_src_paths=$(echo should-pass/*/exe.dk)
-
-for exe_src_path in $exe_src_paths; do
-    dir=$(dirname $exe_src_path);
-    printf "\t\$(MAKE) --directory \$(dir $dir/exe) \$@\n"
+for exe_src_path in should-pass/*/exe.dk; do
+    printf "\t\$(MAKE) --directory %s \$@\n" $(dirname $exe_src_path)
 done
