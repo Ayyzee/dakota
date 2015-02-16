c1339343d1c18102362e2d6e6e0ef0bc5bc9cb04
Robert Nielsen <robert@dakota.org>
2015-02-15 16:37:37 -0800

    Updated test/TEMPLATES-exe-{0,1,2,3}/project.rep. Improved Makefile to better support the should-fail/ tests as well as should-pass/ tests.

--- test/generate-phony-targets-1.sh
+++ test/generate-phony-targets-1.sh
@@ -4,7 +4,7 @@ set -o nounset -o errexit -o pipefail
 
 echo "$1: \\"
 
-for exe_src_path in should-pass/*/exe.dk; do
+for exe_src_path in $2/*/exe.dk; do
     dir=$(dirname $exe_src_path)
     echo " $dir/exe \\"
 done
--- test/generate-phony-targets-2.sh
+++ test/generate-phony-targets-2.sh
@@ -4,6 +4,6 @@ set -o nounset -o errexit -o pipefail
 
 echo "$1:"
 
-for exe_src_path in should-pass/*/exe.dk; do
+for exe_src_path in $2/*/exe.dk; do
     printf "\t\$(MAKE) --directory %s \$@\n" $(dirname $exe_src_path)
 done
