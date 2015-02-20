718d2816007c96b9e0dfc3a6aec74248bae37baf
Robert Nielsen <robert@dakota.org>
2015-02-19 20:06:19 -0800

    Created test/bin/ and populated it.

--- test/bin/generate-phony-targets-1.sh
+++ test/bin/generate-phony-targets-1.sh
@@ -2,14 +2,14 @@
 
 set -o nounset -o errexit -o pipefail
 
-source common.sh
+source bin/common.sh
 
 dk_paths=$(paths-from-pattern "$2/*/exe.dk")
 cc_paths=$(paths-from-pattern "$2/*/exe.cc")
 
-echo "$1:"
+echo "$1: \\"
 
 for exe_src_path in $dk_paths $cc_paths; do
     dir=$(dirname $exe_src_path)
-    printf "\t\$(MAKE) --directory $dir \$@\n"
+    echo " $dir/exe \\"
 done
--- test/bin/generate-phony-targets-2.sh
+++ test/bin/generate-phony-targets-2.sh
@@ -2,14 +2,14 @@
 
 set -o nounset -o errexit -o pipefail
 
-source common.sh
+source bin/common.sh
 
 dk_paths=$(paths-from-pattern "$2/*/exe.dk")
 cc_paths=$(paths-from-pattern "$2/*/exe.cc")
 
-echo "$1: \\"
+echo "$1:"
 
 for exe_src_path in $dk_paths $cc_paths; do
     dir=$(dirname $exe_src_path)
-    echo " $dir/exe \\"
+    printf "\t\$(MAKE) --directory $dir \$@\n"
 done
