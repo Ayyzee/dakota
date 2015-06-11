d79ee140c22b205ff734bae062b31b434d370f46
Robert Nielsen <robert@dakota.org>
2015-06-10 23:47:40 -0700

    Minor update.

--- src/bin/file-depend.pl
+++ src/bin/file-depend.pl
@@ -29,6 +29,7 @@ BEGIN {
 sub expand {
   my ($str) = @_;
   $str =~ s/(\$\w+)/$1/eeg;
+  #$str =~ s/(\$\w+)/$1/eg;
   return $str;
 }
 
@@ -37,8 +38,7 @@ sub expand_tbl {
   my ($key, $val);
 
   while (($key, $val) = each (%$tbl_in)) {
-    $val = &var_make_to_perl($val);
-    $val = &expand($val);
+    $val = &expand(&var_perl_from_make($val));
     $$tbl_out{$key} = $val;
   }
   #print &Dumper($tbl_out);
@@ -56,33 +56,37 @@ sub start {
   my ($argv) = @_;
   my $pattern_name = 'rep-from-so';
   my $path_in = 'foo/bar.$(so_ext)';
-  #print 'pattern-name: ' . $pattern_name . "\n";
-  #print 'in:  ' . $path_in . "\n";
+  print 'pattern-name: ' . $pattern_name . "\n";
+  print 'in:  ' . $path_in . "\n";
   my $path_out = &path_out_from_path_in($pattern_name, $path_in);
   print 'out: ' . $path_out . "\n";
 }
-
-sub var_make_to_perl { # convert variable syntax from make to perl
+sub var_perl_from_make { # convert variable syntax to perl from make
   my ($str) = @_;
   my $result = $str;
   $result =~ s|\$\((\w+)\)|\$$1|g;
+  $result =~ s|\$\{(\w+)\}|\$$1|g;
   return $result;
+}
+sub rep_path_from_so_path {
+
 }
 sub path_out_from_path_in {
   my ($pattern_name, $path_in) = @_;
- #print 'pattern: ' . $$patterns{$pattern_name} . "\n";
+  print 'pattern: ' . $$patterns{$pattern_name} . "\n";
   if (!$expanded_patterns) {
     $expanded_patterns = &expand_tbl($patterns, {});
   }
   my $pattern = $$expanded_patterns{$pattern_name};
- #print 'pattern: ' . $pattern . "\n";
-  my $result = &expand(&var_make_to_perl($path_in));
- #print 'in:  ' . $result . "\n";
+  print 'pattern: ' . $pattern . "\n";
+  my $result = &expand(&var_perl_from_make($path_in));
+  print 'in:  ' . $result . "\n";
   my ($pattern_replacement, $pattern_template) = split(/\s*:\s*/, $pattern);
   $pattern_template =~ s|\%|(\.+?)|;
 
  #$pattern_replacement =~ s|\%|&canon_path(\$1)|;
  #$pattern_replacement =~ s|\%|\$1|;
+ #$pattern_replacement =~ s|\%|&canon_path(\%s)|;
   $pattern_replacement =~ s|\%|\%s|;
 
  #print STDERR "DEBUG: $pattern_template  ->  $pattern_replacement\n";
@@ -90,7 +94,8 @@ sub path_out_from_path_in {
 
   if (1) {
     if ($result =~ m|^$pattern_template$|) {
-      $result = sprintf($pattern_replacement, $1);
+      $result = sprintf($pattern_replacement, &canon_path($1));
+      $result = &expand($result);
     }
   } else {
     #my $result0 = $path_in =~ s|^(.+?)\.so$|obj/$1.rep|r;
