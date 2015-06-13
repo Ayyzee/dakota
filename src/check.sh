5a6068295c09ca5d562194c97d0563b09240a19b
Robert Nielsen <robert@dakota.org>
2015-06-12 20:06:50 -0700

    Initial commit of src/bin/lslt.

--- src/check.sh
+++ src/check.sh
@@ -3,31 +3,10 @@
 set -o nounset -o errexit -o pipefail
 
 objdir=obj
-so_ext=dylib # darwin
+so_ext=dylib # linux: so_ext=so, darwin: so_ext=dylib
 
-function lslt
-{
-  return_value=0
-  for file in "$@"; do
-    #size=`stat -c%s $file 2>/dev/null` # linux
-    #if [[ $? -eq 0 ]]; then
-    #  echo $size $file
-    #  printf "% 9i %s\n" $size $file
-    #else
-    #  return_value=1
-    #fi
-
-    eval $(stat -s $file) # darwin
-    if [[ $? -eq 0 ]]; then
-      printf "% 9i %s\n" $st_size $file
-    else
-      return_value=1
-    fi
-  done
-  return $return_value
-}
-lslt $objdir/dakota/lib/libdakota.dylib.ctlg.rep\
-     $objdir/dakota/lib/libdakota.rep
+./bin/lslt $objdir/dakota/lib/libdakota.$so_ext.ctlg.rep\
+           $objdir/dakota/lib/libdakota.rep
 echo
-lslt $objdir/dakota/lib/libdakota-util.dylib.ctlg.rep\
-     $objdir/dakota/lib/libdakota-util.rep
+./bin/lslt $objdir/dakota/lib/libdakota-util.$so_ext.ctlg.rep\
+           $objdir/dakota/lib/libdakota-util.rep
