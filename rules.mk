55a97a2139c9eea4a3b6713d10ee64e4d02d50e1
Robert Nielsen <robert@dakota.org>
2015-06-17 20:37:43 -0700

    Fixed broken install.

--- rules.mk
+++ rules.mk
@@ -14,8 +14,6 @@ $(objdir)/%.tbl: $(srcdir)/%.sh
 
 CXX_INCLUDE_DIRECTORY_FLAGS := --include-directory
 
-$(blddir)/../bin/%: $(srcdir)/%-main.cc
-	$(CXX) $(CXXFLAGS) $(EXTRA_CXXFLAGS) $(CXX_WARNINGS_FLAGS) $(CXX_INCLUDE_DIRECTORY_FLAGS) ../include $(CXX_OUTPUT_FLAGS) $@ $^
 $(blddir)/%: $(srcdir)/%-main.cc
 	$(CXX) $(CXXFLAGS) $(EXTRA_CXXFLAGS) $(CXX_WARNINGS_FLAGS) $(CXX_INCLUDE_DIRECTORY_FLAGS) ../include $(CXX_OUTPUT_FLAGS) $@ $^
 
@@ -26,8 +24,6 @@ $(blddir)/%: $(srcdir)/%-main.dk
 
 $(blddir)/../lib/%.$(so_ext):
 	$(DAKOTA) --shared $(DAKOTAFLAGS) $(EXTRA_DAKOTAFLAGS) $(macros) $(include_dirs) --soname $(soname) --output $@ $^
-$(blddir)/%.$(so_ext):
-	$(DAKOTA) --shared $(DAKOTAFLAGS) $(EXTRA_DAKOTAFLAGS) --include-directory ../include --output $@ $^
 
 $(DESTDIR)$(prefix)/bin/%: $(srcdir)/../bin/%
 	sudo $(INSTALL_PROGRAM) $< $(@D)
