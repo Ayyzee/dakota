c4d6c61520695ec55ea54d684b4c382348240cee
Robert Nielsen <robert@dakota.org>
2015-07-12 01:44:24 -0700

    Minor improvements to potential symbol/hash/keyword setting.

--- src/proofs/symbol-addrs-strs/exe.cc
+++ src/proofs/symbol-addrs-strs/exe.cc
@@ -1,8 +1,15 @@
-#define ALEN(a) (sizeof(a)/sizeof(a[0]))
+#include <cstddef>
+#include <cstdio>
+
+#define COUNTOF(a) (sizeof(a)/sizeof(a[0]))
 #define noexport __attribute__((__visibility__("hidden")))
 
 typedef char const* symbol_t;
 
+template <typename T, size_t N>
+constexpr size_t countof(T(&)[N]) {
+  return N;
+}
 namespace __symbol {
   extern noexport symbol_t _first;
   extern noexport symbol_t _last;
@@ -11,7 +18,7 @@ namespace __symbol {
   noexport symbol_t _first;
   noexport symbol_t _last;
 }
-#define CONST const
+#define CONST
 static symbol_t CONST* const symbol_addrs[] = { // read-write
   &__symbol::_first,
   &__symbol::_last,
@@ -20,7 +27,7 @@ static char const* const symbol_strs[] = { // read-only
   "first",
   "last",
 };
-static_assert(ALEN(symbol_addrs) == ALEN(symbol_strs), "symbol_addrs and symbol_strs must be same len");
+static_assert(COUNTOF(symbol_addrs) == COUNTOF(symbol_strs), "symbol_addrs and symbol_strs must be same len");
 symbol_t dk_intern(char const* str);
 symbol_t dk_intern(char const* str) {
   return str;
@@ -34,5 +41,6 @@ void dk_intern(symbol_t CONST* const addrs[], char const* const strs[], unsigned
 }
 int main() {
   dk_intern(symbol_addrs, symbol_strs, sizeof(symbol_addrs)/sizeof(symbol_strs));
+  printf("countof=%zu\n", countof(symbol_addrs));
   return 0;
 }
