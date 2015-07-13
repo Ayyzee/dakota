2f6a31e420e6d5eb53d8bd52648c3150dbddb5d4
Robert Nielsen <robert@dakota.org>
2015-07-13 03:09:58 -0700

    Finally got set-read-only() working

---
+++ src/proofs/set-read-only/exe.cc
@@ -0,0 +1,10 @@
+#include "set-read-only.hh"
+
+int main() {
+  dk_intern(symbol_addrs, symbol_strs, symbol_len);
+#if 1
+  set_read_only("__DATA", "__dk_readonly");
+#endif
+  __symbol::_first = ""; // causes SIGBUS or SIGSEGV
+  return 0;
+}
