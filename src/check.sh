611a627761a993bd8f8f2eb3487d8b846cad3153
Robert Nielsen <robert@dakota.org>
2015-07-17 14:41:25 -0700

    Added check history log files to be able to look back in time.

--- src/check.sh
+++ src/check.sh
@@ -4,7 +4,11 @@ set -o nounset -o errexit -o pipefail
 
 objdir=obj
 so_ext=dylib # linux: so_ext=so, darwin: so_ext=dylib
+mkdir -p check-history
+rm -f check-history/$$.txt
+touch check-history/$$.txt
 
+{
 ./bin/lslt $objdir/dakota/lib/libdakota.$so_ext.rep\
            $objdir/dakota/lib/libdakota.rep
 echo
@@ -12,3 +16,6 @@ echo
            $objdir/dakota/lib/libdakota-util.rep
 echo
 bin/build-file-size-summary.pl
+} > check-history/$$.txt
+
+cat check-history/$$.txt
