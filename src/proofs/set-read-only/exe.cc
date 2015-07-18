3c6836cb37826f6410eba034f00c33307aed6f46
Robert Nielsen <robert@dakota.org>
2015-07-18 14:06:34 -0700

    Added previous sigaction chaining.

--- src/proofs/set-read-only/exe.cc
+++ src/proofs/set-read-only/exe.cc
@@ -11,11 +11,18 @@
 #define handle_error(msg) \
   do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
+static struct sigaction prev_sa[NSIG];
+
 static void
 handler(int sig, siginfo_t *si, void *unused) {
   printf("caught sig%s at %p\n",
          sys_signame[sig], si->si_addr);
-  exit(EXIT_FAILURE);
+  if (nullptr == prev_sa[sig].sa_sigaction) {
+    exit(EXIT_FAILURE);
+  } else {
+    prev_sa[sig].sa_sigaction(sig, si, unused);
+  }
+  return;
 }
 
 int
@@ -38,7 +45,8 @@ main() {
   //for ( ?type ?ident : ( ?type [] ) { ?block-in } )
 
   for (int sig : (int []){ SIGBUS, SIGSEGV }) {
-    e = sigaction(sig,  &sa, NULL); if (-1 == e) handle_error("sigaction");
+    prev_sa[sig].sa_sigaction = nullptr;
+    e = sigaction(sig,  &sa, &prev_sa[sig]); if (-1 == e) handle_error("sigaction");
   }
   __symbol::_first = ""; // causes SIGBUS or SIGSEGV
   return 0;
