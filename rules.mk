6fe5a02b5f52d78a52635644e752dfd64d55afcc
Robert Nielsen <robert@dakota.org>
2015-09-17 23:51:02 -0700

    Fixed install target.

--- rules.mk
+++ rules.mk
@@ -20,17 +20,27 @@ $(blddir)/%: $(srcdir)/%-main.dk
 $(blddir)/../lib/%.$(so_ext):
 	$(DAKOTA) --shared $(DAKOTAFLAGS) $(EXTRA_DAKOTAFLAGS) $(macros) $(include-dirs) --soname $(soname) --output $@ $(libs) $(srcs)
 
-$(DESTDIR)$(prefix)/bin/%: $(srcdir)/../bin/%
-	sudo $(INSTALL_PROGRAM) $< $(@D)
-$(DESTDIR)$(prefix)/bin/%: $(blddir)/../bin/%
-	sudo $(INSTALL_PROGRAM) $< $(@D)
+$(DESTDIR)$(prefix)/lib/dakota/%.json: $(srcdir)/../lib/dakota/%.json
+	sudo $(INSTALL_DATA) $< $(@D)
+$(DESTDIR)$(prefix)/lib/dakota/%.json: $(blddir)/../lib/dakota/%.json
+	sudo $(INSTALL_DATA) $< $(@D)
+
+$(DESTDIR)$(prefix)/lib/dakota/%.pm: $(srcdir)/../lib/dakota/%.pm
+	sudo $(INSTALL_LIB) $< $(@D)
+$(DESTDIR)$(prefix)/lib/dakota/%.pm: $(blddir)/../lib/dakota/%.pm
+	sudo $(INSTALL_LIB) $< $(@D)
 
 $(DESTDIR)$(prefix)/lib/%: $(srcdir)/../lib/%
 	sudo $(INSTALL_LIB) $< $(@D)
 $(DESTDIR)$(prefix)/lib/%: $(blddir)/../lib/%
 	sudo $(INSTALL_LIB) $< $(@D)
 
-$(DESTDIR)$(prefix)/%: $(srcdir)/../%
+$(DESTDIR)$(prefix)/include/%: $(srcdir)/../include/%
 	sudo $(INSTALL_DATA) $< $(@D)
-$(DESTDIR)$(prefix)/%: $(blddir)/../%
+$(DESTDIR)$(prefix)/include/%: $(blddir)/../include/%
 	sudo $(INSTALL_DATA) $< $(@D)
+
+$(DESTDIR)$(prefix)/bin/%: $(srcdir)/../bin/%
+	sudo $(INSTALL_PROGRAM) $< $(@D)
+$(DESTDIR)$(prefix)/bin/%: $(blddir)/../bin/%
+	sudo $(INSTALL_PROGRAM) $< $(@D)
