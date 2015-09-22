4889ad9e3ac336b70b32976b0b8a55c689954cf3
Robert Nielsen <robert@dakota.org>
2015-09-22 16:37:52 -0700

    Added lots of files to src/doc/.

---
+++ src/doc/generic-function-uses.cc
@@ -0,0 +1,6 @@
+method object-t init(object-t self, ...);
+method object-t va::init(object-t self, va-list-t my-va-list);
+
+= dk::    init(o, ...);
+= dk::va::init(o, my-va-list);
+=     va::init(o, my-va-list)
