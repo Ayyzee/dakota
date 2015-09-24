197b2a10f719d37fcc040773153109cd8098d94f
Robert Nielsen <robert@dakota.org>
2015-09-24 14:41:05 -0700

    Cleaned up src/proofs/koenig-lookup.

--- src/proofs/koenig-lookup/exe.cc
+++ src/proofs/koenig-lookup/exe.cc
@@ -2,9 +2,11 @@
 namespace object { struct slots_t; }
 typedef   object::slots_t* object_t;
 namespace object { struct slots_t { object_t klass; }; }
+namespace object { extern object_t klass; }
 namespace object { object_t klass; }
 
-namespace object { slots_t* unbox(object_t object) // Koenig lookup does not allow this???
+namespace object { slots_t* unbox(object_t object); } // Koenig lookup does not allow this???
+namespace object { slots_t* unbox(object_t object)    // Koenig lookup does not allow this???
 {
   return object;
 }}
@@ -12,8 +14,10 @@ namespace object { slots_t* unbox(object_t object) // Koenig lookup does not all
 // klass
 namespace klass { struct slots_t; }
 namespace klass { struct slots_t { void* methods; unsigned int methods_len; unsigned int offset; }; }
+namespace klass { extern object_t klass; }
 namespace klass { object_t klass; }
 
+namespace klass { slots_t* unbox(object_t object); }
 namespace klass { slots_t* unbox(object_t object)
 {
   slots_t* s = 0;
@@ -25,8 +29,10 @@ namespace klass { slots_t* unbox(object_t object)
 // string
 namespace string { struct slots_t; }
 namespace string { struct slots_t { const char* cstr; unsigned int cstr_len; }; }
+namespace string { extern object_t klass; }
 namespace string { object_t klass; }
 
+namespace string { slots_t* unbox(object_t object); }
 namespace string { slots_t* unbox(object_t object)
 {
   slots_t* s = 0;
