e7e153262ef4d3c9a48a10f3adfbed5e03f4c8d7
Robert Nielsen <robert@dakota.org>
2015-10-02 23:12:45 -0700

    Commented out of unrequired rtti demangle code.

--- src/proofs/cxx-typeinfo-demangle/typeinfo.hh
+++ src/proofs/cxx-typeinfo-demangle/typeinfo.hh
@@ -6,8 +6,8 @@
 
 std::string demangle(const char* name);
 
-template <class T>
-std::string type(const T& t) {
-  return demangle(typeid(t).name());
-}
+// template <class T>
+// std::string type(const T& t) {
+//   return demangle(typeid(t).name());
+// }
 #endif
