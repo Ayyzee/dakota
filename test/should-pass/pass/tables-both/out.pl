fdd2d5685cc13176213f5fe39e9561c1456e42b0
Robert Nielsen <robert@dakota.org>
2015-10-07 16:01:39 -0700

    Big fix of table/pair and their application to hashed-set and sorted-set. Fixed json-object-output-stream issues.

---
+++ test/should-pass/pass/tables-both/out.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+# -*- mode: cperl -*-
+# -*- cperl-close-paren-offset: -2 -*-
+# -*- cperl-continued-statement-offset: 2 -*-
+# -*- cperl-indent-level: 2 -*-
+# -*- cperl-indent-parens-as-block: t -*-
+# -*- cperl-tab-always-indent: t -*-
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+$Getopt::Long::ignorecase = 0;
+
+my $opts = {};
+&GetOptions($opts, 'output=s');
+my $fh;
+
+open($fh, "<", "/usr/share/dict/words") || die;
+
+my $tbl = {};
+
+while (<$fh>) {
+  chomp;
+  $$tbl{lc($_)} = uc($_);
+}
+#use Data::Dumper;
+#print &Dumper($tbl);
+
+print "struct str_pair_t { char const* first; char const* last; };\n";
+print "\n";
+print "static str_pair_t str_pair[] = {\n";
+
+my $i = 0;
+while (my ($first, $last) = each (%$tbl)) {
+  if ($i == 16) { last; }
+  $i++;
+  print "  { .first = \"$first\", .last = \"$last\" },\n";
+}
+print "};\n";
