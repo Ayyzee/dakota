4020a518e2f981814aee931554797aea71366f1d
Robert Nielsen <robert@dakota.org>
2015-10-06 20:15:55 -0700

    Added . to the include directory search path.

--- test/vars.mk
+++ test/vars.mk
@@ -3,5 +3,7 @@ prefix := /usr/local
 include $(rootdir)/config.mk
 include $(rootdir)/vars.mk
 
+EXTRA_CXXFLAGS += --include-directory .
+
 DAKOTA := DK_NO_CONVERT_DASH_SYNTAX=0 DK_ENABLE_TRACE_MACROS=1 $(prefix)/bin/dakota --define-macro DK_ENABLE_TRACE_MACROS=1 --define-macro $(HOST_OS)
 DAKOTA_INFO := $(prefix)/bin/dakota-info
