2e500d4dced9eae4733da35a4d131b6cf0ffcdbf
Robert Nielsen <robert@dakota.org>
2015-10-17 16:50:18 -0700

    Reformatted a bit. Whitespace only changes.

--- src/spawn.cc
+++ src/spawn.cc
@@ -15,10 +15,10 @@ spawn(const char* arg) {
   pipe(errno_pipe);
   char child_errno_str[3 + (1)] = "";
   pid_t pid = fork();
-  if (-1 == pid) return -1;
+  if (-1 == pid)
+    return -1;
   
-  if (0 == pid)
-  { // child
+  if (0 == pid) { // child
     close(errno_pipe[0]); // close read side of pipe
     char* args[] = { const_cast<char*>(arg), nullptr };
     execv(args[0], args);
@@ -29,41 +29,40 @@ spawn(const char* arg) {
     n = write(errno_pipe[1], child_errno_str, sizeof(child_errno_str) - 1);
     assert(sizeof(child_errno_str) - 1 == n);
     exit(EXIT_FAILURE); // execv() returns on on failure
-  }
-  else
-  { // parent
+  } else { // parent
     close(errno_pipe[1]); // close write side of pipe
     int status;
     int child_pid = waitpid(pid, &status, 0);
-    if (-1 == child_pid) return -1;
+    if (-1 == child_pid)
+      return -1;
     if (WIFEXITED(status)) {
       int exit_status = WEXITSTATUS(status);
       if (EXIT_SUCCESS != exit_status) {
-	ssize_t n;
-	n = add_nonblock(errno_pipe[0]);
-	if (-1 == n) std::abort();
-	n = read(errno_pipe[0], child_errno_str, sizeof(child_errno_str) - 1);
-	if (-1 != n) {
-	  if (sizeof(child_errno_str) - 1 == n) {
-	    child_errno_str[sizeof(child_errno_str) - 1] = NUL;
-	    int child_errno;
-	    sscanf(child_errno_str, "%i", &child_errno);
-	    errno = child_errno;
-	    return -1;
-	  }
-	  else // we expect exactly sizeof(child_errno_str) - 1 bytes
-	    std::abort();
-	}
-	else if (EAGAIN != errno)
-	  std::abort();
+        ssize_t n;
+        n = add_nonblock(errno_pipe[0]);
+        if (-1 == n)
+          std::abort();
+        n = read(errno_pipe[0], child_errno_str, sizeof(child_errno_str) - 1);
+        if (-1 != n) {
+          if (sizeof(child_errno_str) - 1 == n) {
+            child_errno_str[sizeof(child_errno_str) - 1] = NUL;
+            int child_errno;
+            sscanf(child_errno_str, "%i", &child_errno);
+            errno = child_errno;
+            return -1;
+          }
+          else // we expect exactly sizeof(child_errno_str) - 1 bytes
+            std::abort();
+        }
+        else if (EAGAIN != errno)
+          std::abort();
       }
       return exit_status;
     }
-    else if (WIFSIGNALED(status))
-    { // needed for x86-64-linux 2.6.20-15
+    else if (WIFSIGNALED(status)) { // needed for x86-64-linux 2.6.20-15
       int sig = WTERMSIG(status);
       if (SIGSEGV == sig)
-	errno = ENOEXEC;
+        errno = ENOEXEC;
       return -1;
     }
     else
