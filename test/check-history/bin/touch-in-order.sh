43c27056587b262b15e50104b899ca1a7662103c
Robert Nielsen <robert@dakota.org>
2015-10-16 23:25:25 -0700

    Updated test/check-history/bin/touch-in-order.sh to deal with the sha1 at the end of every line of the path files (should-pass/pass.txt, ...).

--- test/check-history/bin/touch-in-order.sh
+++ test/check-history/bin/touch-in-order.sh
@@ -2,7 +2,13 @@
 
 set -o errexit -o nounset -o pipefail
 
+skip=0
 for path in $(cat check-history/should-pass/pass.txt); do
-    touch $path
-    sleep 2
+    if [[ 0 != $skip ]]; then
+        skip=0
+    else
+        touch $path
+        sleep 2
+        skip=1
+    fi
 done
