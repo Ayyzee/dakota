e181a4402db1ff9f1b3e5728c729d47917b4c888
Robert Nielsen <robert@dakota.org>
2015-10-18 06:32:33 -0700

    Several minor fixes to tests.

--- test/should-pass/pass/nested-switches-containing-ints-then-strings/check.sh
+++ test/should-pass/pass/nested-switches-containing-ints-then-strings/check.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset -o pipefail
 
-line=$(grep -n 'dkt_hash_switch(int1)' obj/exe-main.cc /dev/null)
+line=$(grep -n 'dk_hash_switch' obj/exe-main.cc /dev/null | grep int1)
 if [[ 0 == $? ]]; then
     echo $line: switch expression should not have been rewritten.
 fi
