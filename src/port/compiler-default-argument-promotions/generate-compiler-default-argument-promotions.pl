971c355a2e12192862f746472d2fabbbb93f1be2
Robert Nielsen <robert@dakota.org>
2015-10-24 17:42:33 -0700

    Renamed src/port/...

--- src/port/compiler-default-argument-promotions/generate-compiler-default-argument-promotions.pl
+++ src/port/compiler-default-argument-promotions/generate-compiler-default-argument-promotions.pl
@@ -15,54 +15,84 @@ my $extra = 2; # NEVER set this to 1
 
 my $type_tbl = {
   'int-t' => {
-    'signed char'       => $extra,
-    'schar-t'           => 1,
+    'signed char'          => $extra,
+    'schar-t'              => 1,
 
-    'schar8-t'          => 1,
-    'schar8::slots-t'   => 1,
+    'schar8-t'             => 1,
+    'schar8::slots-t'      => 1,
 
-    'int8-t'            => 1,
-    'int8::slots-t'     => 1,
+    'int8-t'               => 1,
+    'int8::slots-t'        => 1,
+    'int16-t'              => 1,
+    'int16::slots-t'       => 1,
+    'int32-t'              => 1,
+    'int32::slots-t'       => 1,
+    'int64-t'              => 1,
+    'int64::slots-t'       => 1,
 
-    'int16-t'           => 1,
-    'int16::slots-t'    => 1,
+    'int-least8-t'         => 1,
+    'int-least8::slots-t'  => 1,
+    'int-least16-t'        => 1,
+    'int-least16::slots-t' => 1,
+    'int-least32-t'        => 1,
+    'int-least32::slots-t' => 1,
+    'int-least64-t'        => 1,
+    'int-least64::slots-t' => 1,
 
-    'int32-t'           => 1,
-    'int32::slots-t'    => 1,
-
-    'int64-t'           => 1,
-    'int64::slots-t'    => 1,
+    'int-fast8-t'          => 1,
+    'int-fast8::slots-t'   => 1,
+    'int-fast16-t'         => 1,
+    'int-fast16::slots-t'  => 1,
+    'int-fast32-t'         => 1,
+    'int-fast32::slots-t'  => 1,
+    'int-fast64-t'         => 1,
+    'int-fast64::slots-t'  => 1,
   },
   'uint-t' => {
-    'bool'              => $extra,
-    'bool-t'            => $extra,
+    'bool'                  => $extra,
+    'bool-t'                => 1,
 
-    'boole-t'           => 1,
-    'boole::slots-t'    => 1,
+    'boole-t'               => 1,
+    'boole::slots-t'        => 1,
 
-    'unsigned char'     => $extra,
-    'uchar-t'           => 1,
+    'unsigned char'         => $extra,
+    'uchar-t'               => 1,
 
-    'uchar8-t'          => 1,
-    'uchar8::slots-t'   => 1,
+    'uchar8-t'              => 1,
+    'uchar8::slots-t'       => 1,
 
-    'char16-t'          => 1,
-    'char16::slots-t'   => 1,
+    'char16-t'              => 1, # yes, unsigned
+    'char16::slots-t'       => 1, # yes, unsigned
 
-    'char32-t'          => 1,
-    'char32::slots-t'   => 1,
+    'char32-t'              => 1, # yes, unsigned
+    'char32::slots-t'       => 1, # yes, unsigned
 
-    'uint8-t'           => 1,
-    'uint8::slots-t'    => 1,
+    'uint8-t'               => 1,
+    'uint8::slots-t'        => 1,
+    'uint16-t'              => 1,
+    'uint16::slots-t'       => 1,
+    'uint32-t'              => 1,
+    'uint32::slots-t'       => 1,
+    'uint64-t'              => 1,
+    'uint64::slots-t'       => 1,
 
-    'uint16-t'          => 1,
-    'uint16::slots-t'   => 1,
+    'uint-least8-t'         => 1,
+    'uint-least8::slots-t'  => 1,
+    'uint-least16-t'        => 1,
+    'uint-least16::slots-t' => 1,
+    'uint-least32-t'        => 1,
+    'uint-least32::slots-t' => 1,
+    'uint-least64-t'        => 1,
+    'uint-least64::slots-t' => 1,
 
-    'uint32-t'          => 1,
-    'uint32::slots-t'   => 1,
-
-    'uint64-t'          => 1,
-    'uint64::slots-t'   => 1,
+    'uint-fast8-t'          => 1,
+    'uint-fast8::slots-t'   => 1,
+    'uint-fast16-t'         => 1,
+    'uint-fast16::slots-t'  => 1,
+    'uint-fast32-t'         => 1,
+    'uint-fast32::slots-t'  => 1,
+    'uint-fast64-t'         => 1,
+    'uint-fast64::slots-t'  => 1,
   },
   'double-t' => {
     'float'             => $extra,
@@ -118,7 +148,7 @@ while (my ($promoted_type, $types) = each(%$type_tbl)) {
     print $output;
     #print "exit-val: " . $exit_val . "\n";
     if ($exit_val) {
-      $$result_tbl{$small_type}{$promoted_type} = 1;
+      $$result_tbl{$small_type} = $promoted_type;
     } else {
       $$unpromoted{$small_type} = 1;
     }
@@ -136,7 +166,7 @@ my $str;
 $str = &Dumper($result_tbl);
 $str =~ s/\{[\s\n]*"/\{ "/gs;
 $str =~ s/1[\s\n]*\}/1 \}/gs;
-my $fn = "default-argument-promotions.pl";
+my $fn = "compiler-default-argument-promotions.json";
 open(my $fh, '>', $fn) or die "Could not open file '$fn' $!";
 print $fh $str;
 close($fh);
