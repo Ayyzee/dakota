743fcc0f9e6d4bfcaeca5108af1d73e70f99d98b
Robert Nielsen <robert@dakota.org>
2015-11-06 23:44:09 -0800

    Minor improvements to test/should-pass/pass/compile-dumped-klasses/.

--- test/should-pass/pass/compile-dumped-klasses/all.sh
+++ test/should-pass/pass/compile-dumped-klasses/all.sh
@@ -4,8 +4,10 @@ set -o errexit -o nounset -o pipefail
 
 rootdir=../../../..
 
+o_ext=bc # clang only
+
 $rootdir/bin/dakota-info $rootdir/lib/libdakota.dylib >      libdakota.dk
 $rootdir/bin/dakota-info $rootdir/lib/libdakota-util.dylib > libdakota-util.dk
 
-$rootdir/bin/dakota --compile --define-macro __darwin__ --output libdakota.dylib      libdakota.dk
-$rootdir/bin/dakota --compile --define-macro __darwin__ --output libdakota-util.dylib $rootdir/lib/libdakota.dylib libdakota-util.dk
+$rootdir/bin/dakota --compile --define-macro __darwin__ --output libdakota.$o_ext      libdakota.dk
+$rootdir/bin/dakota --compile --define-macro __darwin__ --output libdakota-util.$o_ext libdakota-util.dk $rootdir/lib/libdakota.dylib
