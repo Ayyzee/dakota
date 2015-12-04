b260ecb9ebf033ff6aae475d2db0327b742f21d0
Robert Nielsen <robert@dakota.org>
2015-12-04 02:17:16 -0800

    Removed unneeded files in src/port/compiler-default-argument-promotions.

--- src/port/compiler-default-argument-promotions/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit -o nounset -o pipefail
-
-#clang++ -std=c++11 --output type-index type-index-main.cc
-
-#clang++ --compile -std=c++11 --warn-varargs --warn-error test.cc
-
-./generate-compiler-default-argument-promotions.pl
