d940c898729e4ac28590db98047e9034f0faf93f
Robert Nielsen <robert@dakota.org>
2015-12-06 18:26:26 -0800

    Modified src/check.sh to warn rather than exit failure when the number of lines in a file are different than they should be. This is often a harmless problem which only warrents a warning.

--- src/check.sh
+++ src/check.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# check that obj/%.cc has one more line than %.dk
-
-
-exit_val=0
-
 paths=$(echo obj/*.cc)
 for ccfile in $paths; do
     base=$(basename $ccfile .cc)
@@ -14,9 +9,6 @@ for ccfile in $paths; do
     dkfile_lines=${dkfile_lines[0]}
     ccfile_lines=${ccfile_lines[0]}
     if (( $dkfile_lines != $ccfile_lines - 1 )); then
-        echo "ERROR: rewriting modified the number of lines in traslation unit: $dkfile: $dkfile_lines != $ccfile_lines - 1" >&2
-        exit_val=1
+        echo "WARNING: rewriting modified the number of lines in traslation unit: $dkfile: $dkfile_lines != $ccfile_lines - 1"
     fi
 done
-
-exit $exit_val
