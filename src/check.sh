dc277ceeec89781ae50d9aa3fc2558248eae3207
Robert Nielsen <robert@dakota.org>
2015-12-07 23:06:47 -0800

    Small cleanup of src/check.sh.

--- src/check.sh
+++ src/check.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
 set -o errexit -o nounset -o pipefail
-
 src="func main(int-t, const str-t[]) -> int-t { return 0; }" 
 echo $src > check.dk
 echo $src > check-util.dk
-../bin/dakota --output check check.dk
-../bin/dakota --output check-util check-util.dk /usr/local/lib/libdakota-util.so
-
 set -o verbose
-
+prefix=/usr/local
+../bin/dakota check.dk
+../bin/dakota check-util.dk $prefix/lib/libdakota-util.so
 ./check
 ./check-util
