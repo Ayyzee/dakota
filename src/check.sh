6ed26c5fdbb25554a8478c1ecb459a197710fee3
Robert Nielsen <robert@dakota.org>
2015-12-15 04:01:17 -0800

    Improved src/check.sh's cross platform execution.

--- src/check.sh
+++ src/check.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
+rootdir=..
+source $rootdir/config.sh
 src="func main(int-t, const str-t[]) -> int-t { return 0; }" 
 echo $src > check.dk
 echo $src > check-util.dk
 rm -f check
 rm -f check-util
 set -o verbose
-prefix=..
+prefix=/usr/local
 $prefix/bin/dakota check.dk
-$prefix/bin/dakota check-util.dk $prefix/lib/libdakota-util.so
+$prefix/bin/dakota check-util.dk $prefix/lib/libdakota-util.$so_ext
 
 ./check
 ./check-util
