4adaef44b196966c70dea3842c74fb8ef5cc5ff6
Robert Nielsen <robert@dakota.org>
2016-02-08 17:40:35 -0800

    Fixed minor bug that allways added 'kw-args-names' to every method/generic tested by is_kw_args_generic().

--- lib/dakota/util.pm
+++ lib/dakota/util.pm
@@ -430,7 +430,10 @@ sub parameter_types_str {
 # [ [ x :: signal ], [ object-t, int64-t, ... ] ]
 sub kw_args_generics_sig {
   my ($generic) = @_;
-  my $keyword_types_len = scalar @{$$generic{'kw-args-names'} ||= []};
+  my $keyword_types_len = 0;
+  if ($$generic{'kw-args-names'} && 0 < scalar @{$$generic{'kw-args-names'}}) {
+    $keyword_types_len = scalar @{$$generic{'kw-args-names'}};
+  }
   my $parameter_types = &deep_copy($$generic{'parameter-types'});
   my $offset = scalar @$parameter_types - $keyword_types_len;
   my $keyword_types = [splice @$parameter_types, 0, $offset];
