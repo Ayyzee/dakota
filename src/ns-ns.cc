f9b8b00504e4ab2243f3def4116754c752ef6510
Robert Nielsen <robert@dakota.org>
2016-02-27 05:06:31 -0800

    Minor updates to temporary ns-ns.cc.

--- src/ns-ns.cc
+++ src/ns-ns.cc
@@ -7,24 +7,34 @@ namespace __signature { namespace va {
 namespace object {
   //namespace __signature { using namespace ::__signature; }
   namespace __signature { namespace va { using namespace ::__signature::va; } }
-  namespace __signature { namespace va {
-    static const char* foo = "object::__signature::va::foo";
-  } }
+  namespace __signature {
+    namespace va { static const char* foo = "object::__signature::va::foo"; }
+  }
 }
-namespace __signature { namespace va {
-  static const char* foo = "__signature::va::foo"; // never used
-  static const char* bar = "__signature::va::bar"; // never used
-} }
-namespace object { namespace va {
-    static func foo(void*, va_list) -> void {
-      static const char* sig = __signature::va::foo;
-      printf("%p: %s\n", sig, sig);
-    }
-    static func bar(void*, va_list) -> void {
+namespace __signature {
+  namespace va { static const char* foo = "__signature::va::foo"; } // never used
+  namespace va { static const char* bar = "__signature::va::bar"; }
+}
+namespace object {
+  namespace va { static func foo(void*, va_list) -> void {
+    static const char* sig = __signature::va::foo;
+    printf("%p: %s\n", sig, sig);
+  } }
+  namespace va { static func bar(void*, va_list) -> void {
     static const char* sig = __signature::va::bar;
     printf("%p: %s\n", sig, sig);
-  }
-} }
+  } }
+}
+// namespace object {
+//   static func foo(void*, ...) -> void {
+//     static const char* sig = __signature::foo;
+//     printf("%p: %s\n", sig, __PRETTY_FUNCTION__);
+//   }
+//   static func bar(void*, ...) -> void {
+//     static const char* sig = __signature::bar;
+//     printf("%p: %s\n", sig, __PRETTY_FUNCTION__);
+//   }
+// }
 
 func main() -> int {
   printf("%p: %s\n",         __signature::va::foo,         __signature::va::foo);
