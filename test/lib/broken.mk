2d15115b2c99e1eb518e20f52a2e7ee1f2064616
Robert Nielsen <robert@dakota.org>
2016-06-24 15:29:58 -0700

    Updated test/lib/Makefile to be more simple.

--- test/lib/broken.mk
+++ test/lib/broken.mk
@@ -1,3 +1,6 @@
 all:
+	$(MAKE) $(MAKEFLAGS) all
 	rm -fr dkt
 	../../bin/dakota --shared --project l1.project; find dkt -type f | sort
+
+#	DK_GENERATE_COMMON_HEADER=0	../../bin/dakota --shared --project l1.project --define-macro DK_GENERATE_COMMON_HEADER=1; find dkt -type f | sort
