c2ebf79615591f6bb1d662429f6561f6dd68c381
Robert Nielsen <robert@dakota.org>
2016-07-25 14:37:50 -0700

    Moved potentially inline func code from sub linkage_unit::generate_klasses() to new sub linkage_unit::generate_klasses_funcs().

--- lib/dakota/generate.pm
+++ lib/dakota/generate.pm
@@ -2767,6 +2767,23 @@ sub klass_part {
     }
   }
 }
+sub linkage_unit::generate_klasses_funcs { # optionally inline
+  my ($scope, $ordered_klass_names) = @_;
+  my $col = '';
+  my $klass_path = [];
+  my $original_scratch_str_ref = &global_scratch_str_ref();
+  my $scratch_str = ''; &set_global_scratch_str_ref(\$scratch_str);
+  my $scratch_str_ref = &global_scratch_str_ref();
+  foreach my $klass_name (sort @$ordered_klass_names) { # ok to sort
+    &linkage_unit::generate_klasses_klass_funcs_klass($scope, $col, $klass_path, $klass_name);
+  }
+  $$scratch_str_ref .= $nl;
+  foreach my $klass_name (sort @$ordered_klass_names) { # ok to sort
+    &linkage_unit::generate_klasses_klass_funcs($scope, $col, $klass_path, $klass_name);
+  }
+  &set_global_scratch_str_ref($original_scratch_str_ref);
+  return $$scratch_str_ref;
+}
 sub linkage_unit::generate_klasses {
   my ($scope, $ordered_klass_names) = @_;
   my $col = '';
@@ -2789,13 +2806,7 @@ sub linkage_unit::generate_klasses {
   }
   $$scratch_str_ref .= $nl;
   $$scratch_str_ref .= &labeled_src_str(undef, "klasses-klass-funcs" . '-' . &suffix());
-  foreach my $klass_name (sort @$ordered_klass_names) { # ok to sort
-    &linkage_unit::generate_klasses_klass_funcs_klass($scope, $col, $klass_path, $klass_name);
-  }
-  $$scratch_str_ref .= $nl;
-  foreach my $klass_name (sort @$ordered_klass_names) { # ok to sort
-    &linkage_unit::generate_klasses_klass_funcs($scope, $col, $klass_path, $klass_name);
-  }
+  $$scratch_str_ref .= &linkage_unit::generate_klasses_funcs($scope, $ordered_klass_names); # optionally inline
   $$scratch_str_ref .= $nl;
   $$scratch_str_ref .= &labeled_src_str(undef, "klasses-klass-funcs-non-inline" . '-' . &suffix());
   foreach my $klass_name (sort @$ordered_klass_names) { # ok to sort
