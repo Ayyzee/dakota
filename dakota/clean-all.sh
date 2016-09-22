4d6b12ec54bf85dd5a3806a2462b64ee2dada645
Robert Nielsen <robert@dakota.org>
2016-09-22 14:53:31 -0400

    Minor changes to clean-all.sh

--- dakota/clean-all.sh
+++ dakota/clean-all.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-IFS=$'\t\n'
 set -o errexit -o nounset -o pipefail
 
 if [[ -e Makefile ]]; then
   make clean || true
 fi
-./cmake-clean.sh
-dakota-build2project dakota.build dakota.project
-dakota-build2cmk dakota.project dakota.cmk
-#
+
+rootdir=..
 dir=.
-cmake -Wdev $dir
-#
+
+if true; then
+  ./cmake-clean.sh
+  $rootdir/bin/dakota-build2project dakota.build dakota.project
+  $rootdir/bin/dakota-build2cmk dakota.project dakota.cmk
+  #
+  cmake -Wdev $dir
+fi
+
 ./build-clean.sh $dir
 echo ++build-all++
 ./build-all.sh $dir
