0dc9e5de8fc55eed3d0645f2c37bb7afefac0af6
Robert Nielsen <robert@dakota.org>
2016-10-31 10:28:49 -0400

    Fixed longstanding (but undected) bug in use of dkt_enable_typeinfo as attribute. It was only exposed when dkt_enable_typeinfo was defined as [[so_export]].

--- include/declare-klass-type.hh
+++ include/declare-klass-type.hh
@@ -4,9 +4,9 @@
 
 # include <dakota-decl.hh>
 
-# define dkt_declare_klass_type_typealias(k, t) namespace k { typealias slots_t = t;                      } typealias k ## _t = k::slots_t
-# define dkt_declare_klass_type_structptr(k)    namespace k { struct [[dkt_enable_typeinfo]] slots_t;     } typealias k ## _t = k::slots_t*
-# define dkt_declare_klass_type_struct(k)       namespace k { struct [[dkt_enable_typeinfo]] slots_t;     } typealias k ## _t = k::slots_t
-# define dkt_declare_klass_type_union(k)        namespace k { union  [[dkt_enable_typeinfo]] slots_t;     } typealias k ## _t = k::slots_t
-# define dkt_declare_klass_type_enum(k, i)      namespace k { enum                           slots_t : i; } typealias k ## _t = k::slots_t
+# define dkt_declare_klass_type_typealias(k, t) namespace k { typealias slots_t = t;                  } typealias k ## _t = k::slots_t
+# define dkt_declare_klass_type_structptr(k)    namespace k { struct dkt_enable_typeinfo slots_t;     } typealias k ## _t = k::slots_t*
+# define dkt_declare_klass_type_struct(k)       namespace k { struct dkt_enable_typeinfo slots_t;     } typealias k ## _t = k::slots_t
+# define dkt_declare_klass_type_union(k)        namespace k { union  dkt_enable_typeinfo slots_t;     } typealias k ## _t = k::slots_t
+# define dkt_declare_klass_type_enum(k, i)      namespace k { enum                       slots_t : i; } typealias k ## _t = k::slots_t
 // not represented: func ptr typealias
