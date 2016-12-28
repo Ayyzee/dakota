8d7be345e6810cbd7ce7a7a83ea846763330f333
Robert Nielsen <robert@dakota.org>
2016-12-28 00:39:23 -0800

    Moved SENTINEL-PTR to RHS of boolean comparison.

--- lib/dakota/generate.pm
+++ lib/dakota/generate.pm
@@ -3851,7 +3851,7 @@ sub generate_kw_args_method_defn {
   #$col = &colin($col);
   $$scratch_str_ref .=
     $col . "const $keyword_t* _keyword_;" . $nl .
-    $col . "while (SENTINEL-PTR != (_keyword_ = va-arg(_args_, $keyword_t*))) {" . &ann(__FILE__, __LINE__) . $nl;
+    $col . "while ((_keyword_ = va-arg(_args_, $keyword_t*)) != SENTINEL-PTR) {" . &ann(__FILE__, __LINE__) . $nl;
   $col = &colin($col);
   $$scratch_str_ref .= $col . "switch (_keyword_->hash) { // hash is a constexpr. its compile-time evaluated." . $nl;
   $col = &colin($col);
