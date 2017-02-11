35e1d798cc8dc440aba42f84286fbcf89f4b3249
Robert Nielsen <robert@dakota.org>
2017-02-10 22:01:32 -0800

    Improvement of rewriting bug (not a full fix though).

--- lib/dakota/rewrite.pm
+++ lib/dakota/rewrite.pm
@@ -342,7 +342,7 @@ sub vars_from_defn {
 }
 sub rewrite_methods {
   my ($filestr_ref, $kw_arg_generics) = @_;
-  $$filestr_ref =~ s|((\[\[.+?\]\])?\s*method\s+($rmid)\((object-t self.*?)\)\s*->\s*(.+?)\s*\{)|&vars_from_defn($1, $3, $4, $kw_arg_generics)|ges;
+  $$filestr_ref =~ s/((\[\[.+?\]\])?\s*method\s+($rmid)\((object-t self.*?)\)\s*->\s*([^(;|\{)]+?)\s*\{)/&vars_from_defn($1, $3, $4, $kw_arg_generics)/ges;
   $$filestr_ref =~ s|(?<=$stmt_boundry)(\s*)\[\[alias\((.+?)\)\]\](\s*)method(\s+)|$1ALIAS($2)$3METHOD$4|gs;
   $$filestr_ref =~ s|(?<=$stmt_boundry)(\s*)(\s*(\[\[.+?\]\])*)(\s*)method(\s+)($id)|$1$2$4METHOD$5$6|gs; #hackhack
   $$filestr_ref =~ s|(?<=$stmt_boundry)(\s*)method(\s+)($id)|$1METHOD$2$3|gs; #hackhack
