f90938662c3b04e32d82217637673e295f7220a5
Robert Nielsen <robert@dakota.org>
2017-07-05 12:28:51 -0700

    Update macro log-method() to use 'param-types' rather than 'params'.

--- include/dakota-log.h
+++ include/dakota-log.h
@@ -26,7 +26,7 @@
 [[so_export]] [[format_va_printf(2)]] FUNC dkt_va_log(int64_t priority, str_t format, va_list_t args) -> int_t;
 [[so_export]] [[format_printf(   2)]] FUNC dkt_log(   int64_t priority, str_t format, ...) -> int_t;
 
-# define log_method()     dkt_log(dkt::k_log_debug, "\"klass\": \"%s\",\"method\": \"%s\",\"params\": \"%s\"", __name__, __method__->name, __method__->param_types)
+# define log_method()     dkt_log(dkt::k_log_debug, "\"klass\": \"%s\",\"method\": \"%s\",\"param-types\": \"%s\"", __name__, __method__->name, __method__->param_types)
 # define log_klass_func() dkt_log(dkt::k_log_debug, "\"klass\": \"%s\",\"func\": \"%s\"", __name__, __func__)
 # define log_func()       dkt_log(dkt::k_log_debug, "\"func\": \"%s\"", __func__)
 
