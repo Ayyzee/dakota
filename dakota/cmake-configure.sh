574c1a804fa1c0244e4371d0be078e4e0179df3f
Robert Nielsen <robert@dakota.org>
2017-07-08 13:27:52 -0700

    Add partial support for out-of-source builds in cmake.

--- dakota/cmake-configure.sh
+++ dakota/cmake-configure.sh
@@ -2,8 +2,8 @@
 set -o errexit -o nounset -o pipefail
 dir=.; if [[ $# == 1 ]]; then dir=$1; fi
 echo $dir > cmake-build-dir.txt
-rootdir=..
-$rootdir/bin/dakota-build2project dakota.build   > dakota.project
-$rootdir/bin/dakota-build2cmk     dakota.project > dakota.cmk
+rootdir=$HOME/dakota
+$rootdir/bin/dakota-build2project $dir/dakota.build   > $dir/dakota.project
+$rootdir/bin/dakota-build2cmk     $dir/dakota.project > $dir/dakota.cmk
 extra_opts="-Wdev -Wdeprecated"
 cmake $extra_opts $dir
