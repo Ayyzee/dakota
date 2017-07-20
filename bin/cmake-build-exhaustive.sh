a226e420d558ac5bcdf62b7805f0ea22abeff195
Robert Nielsen <robert@dakota.org>
2017-07-19 22:25:44 -0700

    Improve cmake-build-exhaustive family of scripts.

--- bin/cmake-build-exhaustive.sh
+++ bin/cmake-build-exhaustive.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 binary_dir=.; if [[ $# == 1 ]]; then binary_dir=$1; fi
+if [[ -e make.sh ]]; then ./make.sh clean; fi
 rootdir=..
 $rootdir/bin/cmake-clean.sh
 $rootdir/bin/cmake-configure.sh $binary_dir # different from autoconf configure
