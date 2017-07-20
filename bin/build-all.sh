307811f4fd2e5b7addbbbf4493c75660859289d1
Robert Nielsen <robert@dakota.org>
2017-07-19 21:55:53 -0700

    Renamed bin/build-all.sh -> bin/build-init-all.sh.

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -2,5 +2,4 @@
 set -o errexit -o nounset -o pipefail
 binary_dir=$(cat cmake-binary-dir.txt)
 jobs=$(getconf _NPROCESSORS_ONLN)
-cmake --build $binary_dir --target init
 cmake --build $binary_dir --target all -- --jobs $jobs
