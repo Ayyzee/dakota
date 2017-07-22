60a1709ea063be3046f516c50c1a2d8fbe25c8b2
Robert Nielsen <robert@dakota.org>
2017-07-22 13:09:56 -0700

    Update cmake build scripts.

--- bin/cmake-build-exhaustive.sh
+++ bin/cmake-build-exhaustive.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
-binary_dir=build-cmk; if [[ $# == 1 ]]; then binary_dir=$1; fi
 if [[ -e make.sh ]]; then ./make.sh clean; fi
 rootdir=..
 $rootdir/bin/cmake-clean.sh
 rm -f dakota.cmake # hackhack
-$rootdir/bin/cmake-configure.sh $binary_dir # different from autoconf configure
+$rootdir/bin/cmake-configure.sh
 $rootdir/bin/build-clean.sh
 time $rootdir/bin/build-all.sh
--- bin/cmake-configure.sh
+++ bin/cmake-configure.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
-rel_source_dir=.
-binary_dir=$rel_source_dir; if [[ $# == 1 ]]; then binary_dir=$1; fi
+binary_dir=build-cmk
+rel_source_dir=..
 echo $binary_dir > cmake-binary-dir.txt
 extra_opts="-Wdev -Wdeprecated"
-
-if [[ $binary_dir != '.' && $binary_dir != '..' ]]; then
-  mkdir -p $binary_dir
-  cd $binary_dir
-  rel_source_dir=..
-fi
+mkdir -p $binary_dir
+cd $binary_dir
 cmake $extra_opts $rel_source_dir
