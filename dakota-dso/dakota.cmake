9c3bce38c6814ca9aa76e54495fe66c36f3ad0cb
Robert Nielsen <robert@dakota.org>
2017-07-31 11:48:09 -0700

    Rename root-dir to root-source-dir.

--- dakota-dso/dakota.cmake
+++ dakota-dso/dakota.cmake
@@ -1,12 +1,12 @@
 # -*- mode: cmake -*-
 set (builddir ${CMAKE_CURRENT_SOURCE_DIR}/build-dkt)
-set (compiler-opts @${root-dir}/lib/dakota/compiler.opts)
+set (compiler-opts @${root-source-dir}/lib/dakota/compiler.opts)
 set (linker-opts   @${CMAKE_CURRENT_SOURCE_DIR}/../lib/dakota/linker.opts)
 set (include-dirs
-  ${root-dir}/include
+  ${root-source-dir}/include
 )
 set (install-include-files
-  ${root-dir}/include/dakota-dso.h
+  ${root-source-dir}/include/dakota-dso.h
 )
 set (is-lib 1)
 set (libs
--- dakota-find-library/dakota.cmake
+++ dakota-find-library/dakota.cmake
@@ -1,13 +1,13 @@
 # -*- mode: cmake -*-
 set (builddir ${CMAKE_CURRENT_SOURCE_DIR}/build-dkt)
-set (compiler-opts @${root-dir}/lib/dakota/compiler.opts)
+set (compiler-opts @${root-source-dir}/lib/dakota/compiler.opts)
 set (linker-opts   @${CMAKE_CURRENT_SOURCE_DIR}/../lib/dakota/linker.opts)
 set (include-dirs
-  ${root-dir}/include
+  ${root-source-dir}/include
   ${CMAKE_INSTALL_PREFIX}/include
 )
 set (lib-dirs
-  ${root-dir}/lib
+  ${root-source-dir}/lib
   ${CMAKE_INSTALL_PREFIX}/lib
 )
 set (libs
