ed4cf0ffddceea5bc0eadde7617ffde3c46123aa
Robert Nielsen <robert@dakota.org>
2017-08-02 20:54:37 -0700

    Add support for build-exhaustive-targets.sh 'config' quasi-target (really it just does a reconfigure).

--- build-exhaustive-targets.sh
+++ build-exhaustive-targets.sh
@@ -4,6 +4,9 @@ export PATH=$HOME/dakota/bin:$PATH
 export DAKOTA_VERBOSE=1
 export CMAKE_VERBOSE_MAKEFILE=ON
 
-#rm -fr $(cat cmake-binary-dir.txt)
-bin/cmake-configure.sh
+if [[ $# -ge 1 && $1 == config ]]; then
+  shift
+  rm -fr $(cat cmake-binary-dir.txt)
+  bin/cmake-configure.sh
+fi
 bin/build.sh $@
