8b300ef647acee6307383ae91f9f46e0b5effb41
Robert Nielsen <robert@dakota.org>
2017-08-04 14:09:40 -0700

    Workaround small build system issue.

--- root-build.sh
+++ root-build.sh
@@ -7,6 +7,7 @@ export CMAKE_VERBOSE_MAKEFILE=ON
 if [[ $# -ge 1 && $1 == config ]]; then
   rm -fr $(cat cmake-binary-dir.txt)
   bin/cmake-configure.sh
+  rm -f */parts.yaml # hackhack
   shift
   if [[ $# -ge 1 ]]; then
     bin/build.sh $@
