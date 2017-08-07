b47e6d689379f5f3db69fc3bbc0de936ee5aaeb6
Robert Nielsen <robert@dakota.org>
2017-08-07 10:50:38 -0700

    Added build duration calculation and echoing.

--- root-build.sh
+++ root-build.sh
@@ -4,13 +4,16 @@ export PATH=$HOME/dakota/bin:$PATH
 export DAKOTA_VERBOSE=1
 export CMAKE_VERBOSE_MAKEFILE=ON
 
+build() {
+  SECONDS=0; bin/build.sh $@; duration=$SECONDS; echo "duration: $(($duration / 60))m$(($duration % 60))s"
+}
 if [[ $# -ge 1 && $1 == config ]]; then
   rm -fr $(cat cmake-binary-dir.txt)
   bin/cmake-configure.sh
   shift
   if [[ $# -ge 1 ]]; then
-    bin/build.sh $@
+    build $@
   fi
 else
-  bin/build.sh $@
+  build $@
 fi
