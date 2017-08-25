693f80237a2b599ee28cd9dc72d032eb700a4e35
Robert Nielsen <robert@dakota.org>
2017-08-25 10:10:12 -0700

    Add top-level targets exe-core and exe to test building of executables.

---
+++ exe/build-vars.cmake
@@ -0,0 +1,24 @@
+# -*- mode: cmake -*-
+set (build-dir ${CMAKE_SOURCE_DIR}/build-dkt/${target})
+set (macros
+)
+set (bin-dirs
+  ${CMAKE_SOURCE_DIR}/bin
+)
+set (include-dirs
+  ${CMAKE_SOURCE_DIR}/include
+)
+set (lib-dirs
+)
+set (libs
+)
+set (target-lib-dirs
+)
+set (target-libs
+  dakota-core
+  dakota
+)
+set (target-type executable)
+set (srcs
+  ${PROJECT_SOURCE_DIR}/exe.dk
+)
