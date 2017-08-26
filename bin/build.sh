3427205b093b1277b2cce9cfaf7b8c5698ce75a8
Robert Nielsen <robert@dakota.org>
2017-08-26 13:20:36 -0700

    Improve verbosity control with ninja and make.

--- bin/build.sh
+++ bin/build.sh
@@ -12,10 +12,24 @@ if [[ -e jobs.txt ]]; then
   jobs=$(cat jobs.txt)
 fi
 export PATH=$HOME/dakota/bin:$PATH
-export DAKOTA_VERBOSE=1
-export CMAKE_VERBOSE_MAKEFILE=ON
 generator="${generator:-ninja}"
+generator_opts=
+if   [[ $generator == ninja ]]; then
+  generator_opts="-C $binary_dir -j $jobs"
+elif [[ $generator == make  ]]; then
+  generator_opts="-C $binary_dir -j $jobs --no-print-directory"
+fi
+verbose="${verbose:-0}"
+if [[ $verbose -ne 0 ]]; then
+  export DAKOTA_VERBOSE=1
+  #export CMAKE_VERBOSE_MAKEFILE=ON
+  if   [[ $generator == ninja ]]; then
+    generator_opts="$generator_opts -v"
+  elif [[ $generator == make  ]]; then
+    generator_opts="$generator_opts VERBOSE=1"
+  fi
+fi
 SECONDS=0
-$generator -C $binary_dir -j $jobs $@
+$generator $generator_opts $@
 duration=$SECONDS
 echo "duration: $(($duration / 60))m$(($duration % 60))s"
