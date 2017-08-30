fd61661cd3ba4b639b564d60dc1dc1857650fa19
Robert Nielsen <robert@dakota.org>
2017-08-30 15:50:53 -0700

    Add comment/code to root-build.sh.

--- root-build.sh
+++ root-build.sh
@@ -3,7 +3,12 @@ set -o errexit -o nounset -o pipefail
 
 if [[ $# -ge 1 && $1 == config ]]; then
   binary_dir=$(cat cmake-binary-dir.txt)
+  if false; then
+    build_dir=$binary_dir/../build-dkt
+    rm -fr $build_dir
+  fi
   rm -fr $binary_dir
+ #rm -fr $binary_dir/{CMakeCache.txt,CMakeFiles,Makefile,build.ninja,rules.ninja}
   bin/cmake-configure.sh
   shift
   if [[ $# -ge 1 ]]; then
