aa05d1eeddc8f109201058c981496ee46761a4fe
Robert Nielsen <robert@dakota.org>
2017-08-30 15:59:06 -0700

    Update trouble.sh script which reproduces strange bug when switching from one generator to another.

--- trouble.sh
+++ trouble.sh
@@ -2,8 +2,12 @@
 set -o errexit -o nounset -o pipefail -o xtrace
 if false; then
   generator=ninja ./root-build.sh config clean
-  generator=make  ./root-build.sh config clean all
+  generator=make  ./root-build.sh config all
 else
-  generator=make  ./root-build.sh config clean
-  generator=ninja ./root-build.sh config clean all
+  generator=make  ./root-build.sh config
+  generator=make  ./root-build.sh clean
+
+  generator=ninja ./root-build.sh config
+ #generator=ninja ./root-build.sh clean
+  generator=ninja ./root-build.sh all
 fi
