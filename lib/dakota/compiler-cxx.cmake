be1ab705332942755e333d1da65e3010a0b9e7eb
Robert Nielsen <robert@dakota.org>
2017-09-03 15:45:19 -0700

    More cleanup of many 'generic' *.cmake files.

--- lib/dakota/compiler-cxx.cmake
+++ lib/dakota/compiler-cxx.cmake
@@ -1,18 +1,18 @@
 # -*- mode: cmake -*-
+set (cxx-compiler ${CMAKE_CXX_COMPILER})
+set (cxx-standard 17)
 set (compiler-opts @${prefix-dir}/lib/dakota/compiler.opts)
 set (linker-opts   @${prefix-dir}/lib/dakota/linker.opts)
-target_compile_options (${target} PRIVATE ${compiler-opts})
+
 set (link-options ${linker-opts})
 dk_join (link-options-str " " ${link-options})
-set_target_properties (${target} PROPERTIES LINK_FLAGS "${link-options-str}")
-#set (CMAKE_LIBRARY_PATH ${prefix-dir}/lib)
-set (cxx-standard 17)
-set (CMAKE_COMPILER_IS_GNUCXX TRUE)
-dk_find_program (cxx-compiler clang++${CMAKE_EXECUTABLE_SUFFIX})
-set (CMAKE_CXX_COMPILER ${cxx-compiler})
-set_source_files_properties (${srcs} PROPERTIES LANGUAGE CXX CXX_STANDARD ${cxx-standard})
-set_target_properties (${target} PROPERTIES LANGUAGE CXX CXX_STANDARD ${cxx-standard})
-set_target_properties (${target} PROPERTIES CXX_VISIBILITY_PRESET hidden)
-#set (CMAKE_CXX_VISIBILITY_PRESET hidden)
+
+set_source_files_properties (${srcs}  PROPERTIES LANGUAGE              CXX
+                                                 CXX_STANDARD          ${cxx-standard})
+set_target_properties (     ${target} PROPERTIES LANGUAGE              CXX
+                                                 CXX_STANDARD          ${cxx-standard}
+                                                 CXX_VISIBILITY_PRESET hidden
+                                                 LINK_FLAGS            "${link-options-str}")
+target_compile_options (    ${target} PRIVATE ${compiler-opts})
 target_compile_definitions (${target} PRIVATE ${macros})
 target_include_directories (${target} PRIVATE ${include-dirs})
