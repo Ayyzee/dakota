879c9e143bc76988479046cfb58f987a2f6a7571
Robert Nielsen <robert@dakota.org>
2017-09-03 15:55:12 -0700

    Clean up the naming and use of 'option files'.

--- lib/dakota/compiler-cxx.cmake
+++ lib/dakota/compiler-cxx.cmake
@@ -1,18 +1,16 @@
 # -*- mode: cmake -*-
 set (cxx-compiler ${CMAKE_CXX_COMPILER})
 set (cxx-standard 17)
-set (compiler-opts @${prefix-dir}/lib/dakota/compiler.opts)
-set (linker-opts   @${prefix-dir}/lib/dakota/linker.opts)
-
-set (link-options ${linker-opts})
-dk_join (link-options-str " " ${link-options})
+set (compiler-opts-file @${prefix-dir}/lib/dakota/compiler.opts)
+set (linker-opts-file   @${prefix-dir}/lib/dakota/linker.opts)
+dk_join (linker-opts-file-str " " ${linker-opts-file})
 
 set_source_files_properties (${srcs}  PROPERTIES LANGUAGE              CXX
                                                  CXX_STANDARD          ${cxx-standard})
 set_target_properties (     ${target} PROPERTIES LANGUAGE              CXX
                                                  CXX_STANDARD          ${cxx-standard}
                                                  CXX_VISIBILITY_PRESET hidden
-                                                 LINK_FLAGS            "${link-options-str}")
-target_compile_options (    ${target} PRIVATE ${compiler-opts})
+                                                 LINK_FLAGS            "${linker-opts-file-str}")
+target_compile_options (    ${target} PRIVATE ${compiler-opts-file})
 target_compile_definitions (${target} PRIVATE ${macros})
 target_include_directories (${target} PRIVATE ${include-dirs})
