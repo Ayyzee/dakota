13d8f81d53abe10e2d3c60f665dd339b628952fe
Robert Nielsen <robert@dakota.org>
2017-09-03 11:55:40 -0700

    Fix hard-coded variable rel_source_dir to use absolute path captured before the cd.

--- bin/cmake-configure.sh
+++ bin/cmake-configure.sh
@@ -5,12 +5,12 @@ if [[ ! -e .binary-dir.txt ]]; then
   echo $binary_dir > .binary-dir.txt
 fi
 binary_dir=$(cat .binary-dir.txt)
-rel_source_dir=../..
 generator_id="${generator_id:-Ninja}"
 if [[ ${generator:-ninja} == make ]]; then
   generator_id="Unix Makefiles"
 fi
 extra_opts="-DCMAKE_BUILD_TYPE=Debug -Wdev -Wdeprecated"
+source_dir=$(pwd)
 mkdir -p $binary_dir
 cd $binary_dir
-cmake -G "$generator_id" $extra_opts $rel_source_dir
+cmake -G "$generator_id" $extra_opts $source_dir
