cdf8d4dfac172d56f33a5565a7bd837d4bb12fea
Robert Nielsen <robert@dakota.org>
2017-09-03 17:58:33 -0700

    Whitespace only changes.

--- lib/dakota/base.cmake
+++ lib/dakota/base.cmake
@@ -10,6 +10,15 @@ execute_process (
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 target_sources (${target} PRIVATE ${target-src})
 
+# generate target-src
+add_custom_command (
+  OUTPUT ${target-src}
+  COMMAND ${CMAKE_CXX_COMPILER} --target-src --parts ${parts}
+  DEPENDS ${parts} ${target-libs} dakota-catalog${CMAKE_EXECUTABLE_SUFFIX}
+  VERBATIM)
+set (compile-defns DKT_TARGET_FILE="${target-output-file}" DKT_TARGET_TYPE="${target-type}")
+set_source_files_properties (${target-src} PROPERTIES COMPILE_DEFINITIONS "${compile-defns}")
+
 set (target-hdr ${target}.target-hdr)
 # phony target 'target-hdr'
 add_custom_target (
@@ -27,12 +36,3 @@ add_custom_command (
     lib-files:  ${target-lib-files} ${lib-files}
     srcs:       ${srcs}
   VERBATIM)
-
-# generate target-src
-add_custom_command (
-  OUTPUT ${target-src}
-  DEPENDS ${parts} ${target-libs} dakota-catalog${CMAKE_EXECUTABLE_SUFFIX}
-  COMMAND ${CMAKE_CXX_COMPILER} --target-src --parts ${parts}
-  VERBATIM)
-set (compile-defns DKT_TARGET_FILE="${target-output-file}" DKT_TARGET_TYPE="${target-type}")
-set_source_files_properties (${target-src} PROPERTIES COMPILE_DEFINITIONS "${compile-defns}")
