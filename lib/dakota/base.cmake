09348fb2771b61a2dff8cd23b6aa782fb108e00c
Robert Nielsen <robert@dakota.org>
2017-09-04 22:05:10 -0700

    Add to bin/dakota support for --var=key=val for passing environment via the command line.

--- lib/dakota/base.cmake
+++ lib/dakota/base.cmake
@@ -3,13 +3,13 @@ include (${prefix-dir}/lib/dakota/base-cxx.cmake)
 set (parts ${current-build-dir}/parts.txt) # invariant
 set (CMAKE_COMPILER_IS_GNUCXX TRUE)
 set (cxx-compiler ${CMAKE_CXX_COMPILER})
-dk_append_target_property (${target} LINK_FLAGS --cxx ${cxx-compiler} --parts ${parts})
-target_compile_options (   ${target} PRIVATE    --cxx ${cxx-compiler} --parts ${parts})
+dk_append_target_property (${target} LINK_FLAGS --cxx ${cxx-compiler} --parts ${parts} --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR} --var=build_dir=${current-build-dir})
+target_compile_options (   ${target} PRIVATE    --cxx ${cxx-compiler} --parts ${parts} --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR} --var=build_dir=${current-build-dir})
 dk_find_program (CMAKE_CXX_COMPILER dakota${CMAKE_EXECUTABLE_SUFFIX})
 dk_find_program (dakota-parts dakota-parts) # ${CMAKE_EXECUTABLE_SUFFIX}
 
 execute_process (
-  COMMAND ${CMAKE_CXX_COMPILER} --target-src --path-only --parts ${parts}
+  COMMAND ${CMAKE_CXX_COMPILER} --target-src --path-only --parts ${parts} --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR} --var=build_dir=${current-build-dir}
   OUTPUT_VARIABLE target-src
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 target_sources (${target} PRIVATE ${target-src})
@@ -17,7 +17,7 @@ target_sources (${target} PRIVATE ${target-src})
 # generate target-src
 add_custom_command (
   OUTPUT ${target-src}
-  COMMAND ${CMAKE_CXX_COMPILER} --target-src --parts ${parts}
+  COMMAND ${CMAKE_CXX_COMPILER} --target-src --parts ${parts} --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR} --var=build_dir=${current-build-dir}
   DEPENDS ${parts} ${target-libs} dakota-catalog${CMAKE_EXECUTABLE_SUFFIX}
   VERBATIM)
 set (compile-defns DKT_TARGET_FILE="${target-output-file}" DKT_TARGET_TYPE="${target-type}")
@@ -28,7 +28,7 @@ set (target-hdr ${target}.target-hdr)
 add_custom_target (
   ${target-hdr}
   DEPENDS ${parts} ${target-libs} dakota-catalog${CMAKE_EXECUTABLE_SUFFIX}
-  COMMAND ${CMAKE_CXX_COMPILER} --target-hdr --parts ${parts}
+  COMMAND ${CMAKE_CXX_COMPILER} --target-hdr --parts ${parts} --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR} --var=build_dir=${current-build-dir}
   VERBATIM)
 add_dependencies (${target} ${target-hdr})
 
