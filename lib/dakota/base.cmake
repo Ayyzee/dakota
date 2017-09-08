c5170d1f3c1b0710ac4264761b31828a4240377e
Robert Nielsen <robert@dakota.org>
2017-09-07 19:59:05 -0700

    Whitespace only changes.

--- lib/dakota/base.cmake
+++ lib/dakota/base.cmake
@@ -2,8 +2,14 @@
 include (${prefix-dir}/lib/dakota/base-cxx.cmake)
 set (CMAKE_COMPILER_IS_GNUCXX TRUE)
 set (cxx-compiler ${CMAKE_CXX_COMPILER})
-dk_append_target_property (${target} LINK_FLAGS --var=cxx=${cxx-compiler} --var=build_dir=${CMAKE_CURRENT_BINARY_DIR} --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR})
-target_compile_options (   ${target} PRIVATE    --var=cxx=${cxx-compiler} --var=build_dir=${CMAKE_CURRENT_BINARY_DIR} --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR})
+dk_append_target_property (${target} LINK_FLAGS
+  --var=build_dir=${CMAKE_CURRENT_BINARY_DIR}
+  --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR}
+  --var=cxx=${cxx-compiler})
+target_compile_options (${target} PRIVATE
+  --var=build_dir=${CMAKE_CURRENT_BINARY_DIR}
+  --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR}
+  --var=cxx=${cxx-compiler})
 dk_find_program (CMAKE_CXX_COMPILER dakota${CMAKE_EXECUTABLE_SUFFIX})
 dk_find_program (dakota-parts dakota-parts) # ${CMAKE_EXECUTABLE_SUFFIX}
 
@@ -22,7 +28,8 @@ add_custom_command (
   VERBATIM)
 
 execute_process (
-  COMMAND ${CMAKE_CXX_COMPILER} --target src --path-only --var=build_dir=${CMAKE_CURRENT_BINARY_DIR}
+  COMMAND ${CMAKE_CXX_COMPILER} --target src --path-only
+    --var=build_dir=${CMAKE_CURRENT_BINARY_DIR}
   OUTPUT_VARIABLE target-src
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 target_sources (${target} PRIVATE ${target-src})
@@ -31,16 +38,22 @@ target_sources (${target} PRIVATE ${target-src})
 add_custom_command (
   OUTPUT ${target-src}
   DEPENDS ${parts} ${target-libs} dakota-catalog${CMAKE_EXECUTABLE_SUFFIX}
-  COMMAND ${CMAKE_CXX_COMPILER} --target src --var=build_dir=${CMAKE_CURRENT_BINARY_DIR} --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR}
+  COMMAND ${CMAKE_CXX_COMPILER} --target src
+    --var=build_dir=${CMAKE_CURRENT_BINARY_DIR}
+    --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR}
   VERBATIM)
-set (compile-defns DKT_TARGET_FILE="${target-output-file}" DKT_TARGET_TYPE="${target-type}")
-set_source_files_properties (${target-src} PROPERTIES COMPILE_DEFINITIONS "${compile-defns}")
+set (compile-defns
+  DKT_TARGET_FILE="${target-output-file}"
+  DKT_TARGET_TYPE="${target-type}")
+set_source_files_properties (${target-src} PROPERTIES
+  COMPILE_DEFINITIONS "${compile-defns}")
 
 set (target-hdr ${target}.target-hdr)
 # phony target 'target-hdr'
-add_custom_target (
-  ${target-hdr}
+add_custom_target (${target-hdr}
   DEPENDS ${parts} ${target-libs} dakota-catalog${CMAKE_EXECUTABLE_SUFFIX}
-  COMMAND ${CMAKE_CXX_COMPILER} --target hdr --var=build_dir=${CMAKE_CURRENT_BINARY_DIR} --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR}
+  COMMAND ${CMAKE_CXX_COMPILER} --target hdr
+    --var=build_dir=${CMAKE_CURRENT_BINARY_DIR}
+    --var=source_dir=${CMAKE_CURRENT_SOURCE_DIR}
   VERBATIM)
 add_dependencies (${target} ${target-hdr})
