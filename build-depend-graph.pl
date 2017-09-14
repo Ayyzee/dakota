4e4480dce6bfef2794520b49f2d32fe3f28dab86
Robert Nielsen <robert@dakota.org>
2017-09-14 15:21:54 -0700

    Improve graph generation.

--- build-depend-graph.pl
+++ build-depend-graph.pl
@@ -20,9 +20,34 @@ $Data::Dumper::Indent =    1;   # default = 2
 my $nl = "\n";
 undef $/;
 
-my $build_dir = 'zzz/build';
-my $target_build_dir = $build_dir . '/z';
-
+### <<
+sub basename {
+  my ($path) = @_;
+  my $name = $path =~ s=^(.*/)*(.+)$=$2=r;
+  return $name;
+}
+sub target_path {
+  my ($name) = @_;
+  return 'b/z/' . &basename($name);
+}
+sub target_srcs_ast_path {
+  return &target_path('srcs.ast');
+}
+sub target_inputs_ast_path {
+  return &target_path('inputs.ast');
+}
+sub target_hdr_path {
+  return &target_path('target.h');
+}
+sub ast_path_from_dk_path {
+  my ($dk_path) = @_;
+  return 'b/' . &basename($dk_path) . '.ast';
+}
+sub ast_path_from_so_path {
+  my ($so_path) = @_;
+  return 'b/' . &basename($so_path) . '.ctlg.ast';
+}
+### >>
 sub add_node {
   my ($current_node,
       $output,
@@ -38,97 +63,100 @@ sub add_node {
   $$current_node{'inputs'}{$output} = $result;
   return $result;
 }
-sub basename {
-  my ($path) = @_;
-  my $name = $path =~ s=^(.*/)*(.+)$=$2=r;
-  return $name;
+sub asts {
+  my ($inputs) = @_;
+  return [ values %$inputs ];
 }
-sub target_path {
-  my ($name) = @_;
-  #return $target_build_dir . '/' . $name;
-  return 'b/z/' . &basename($name);
-}
-sub ast_path_from_dk_path {
-  my ($dk_path) = @_;
-  #return $build_dir . '/' . &basename($dk_path) . '.ast';
-  return 'b/' . &basename($dk_path) . '.ast';
-}
-sub ast_path_from_so_path {
-  my ($so_path) = @_;
-  #return $build_dir . '/' . &basename($so_path) . '.ctlg.ast';
-  return 'b/' . &basename($so_path) . '.ctlg.ast';
-}
-sub dump_dot {
-  my ($node) = @_;
-  my $output = $$node{'output'};
-  foreach my $input (keys %{$$node{'inputs'}}) {
-    if ($$node{'inputs'}{$input}) {
-      &dump_dot($$node{'inputs'}{$input});
-    }
-    if ($output) {
-      print "  \"$output\" -> \"$input\";" . $nl;
+sub gen_target_hdr_graph {
+  my ($inputs) = @_;
+  my $root = {};
+  my ($srcs, $libs) = ({}, {});
+  foreach my $input (sort @$inputs) {
+    $input = &basename($input);
+    if ($input =~ /\.dk$/) {
+      $$srcs{$input} = &ast_path_from_dk_path($input);
+    } else {
+      $$libs{$input} = &ast_path_from_so_path($input);
     }
   }
+  my $target_hdr_node = &add_node($root,
+                                  &target_hdr_path(),
+                                  [ &target_inputs_ast_path() ],
+                                  [ '<cmd>', '--target-hdr', '--output', &target_hdr_path(),
+                                    &target_inputs_ast_path() ]);
+  ###
+  my $inputs_ast_node = &add_node($target_hdr_node,
+                                  &target_inputs_ast_path(),
+                                  [ &target_srcs_ast_path(), @{&asts($libs)} ],
+                                  [ '<cmd>', '--merge-ast', '--output', &target_inputs_ast_path(),
+                                    &target_srcs_ast_path(), @{&asts($libs)} ]);
+  while (my ($lib, $lib_ast) = each %$libs) {
+    &add_node($inputs_ast_node,
+              $lib_ast,
+              [ $lib ],
+              [ '<cmd>', '--parse-lib', '--output', $lib_ast, $lib ]);
+  }
+  ###
+  my $srcs_ast_node = &add_node($inputs_ast_node,
+                                &target_srcs_ast_path(),
+                                [ @{&asts($srcs)} ],
+                                [ '<cmd>', '--merge-ast', '--output', &target_srcs_ast_path(),
+                                  @{&asts($srcs)} ]);
+  while (my ($src, $src_ast) = each %$srcs) {
+    &add_node($srcs_ast_node,
+              $src_ast,
+              [ $src ],
+              [ '<cmd>', '--parse-src', '--output', $src_ast, $src ]);
+  }
+  return $root;
 }
-
 my $inputs = [split(/\s+/, <STDIN>)];
-my $srcs = [];
-my $libs = [];
+my $root = &gen_target_hdr_graph($inputs);
 
-foreach my $input (@$inputs) {
-  $input = &basename($input);
-  if ($input =~ /\.dk$/) {
-    push @$srcs, $input;
-  } else {
-    push @$libs, $input;
+if (1) {
+  sub dump_dot {
+    my ($node) = @_;
+    my $result = '';
+    my $output = $$node{'output'};
+    foreach my $input (sort keys %{$$node{'inputs'}}) {
+      if ($$node{'inputs'}{$input}) {
+        $result .= &dump_dot($$node{'inputs'}{$input});
+      } else {
+        $result .= "  \"$input\" [ color = blue ];" . $nl;
+      }
+      if ($output) {
+        $result .= "  \"$output\" -> \"$input\";" . $nl;
+      }
+    }
+    return $result;
   }
-}
+  sub dump_make {
+    my ($node) = @_;
+    my $result = '';
+    my $output = $$node{'output'};
+    if ($output) {
+      $result .= "$output:";
+      foreach my $input (sort keys %{$$node{'inputs'}}) {
+        if ($output) {
+          $result .= " $input";
+        }
+      }
+      $result .= $nl;
+      $result .= "\t" . join(' ', @{$$node{'cmd'}}) . $nl;
+    }
+    foreach my $input (sort keys %{$$node{'inputs'}}) {
+      if ($$node{'inputs'}{$input}) {
+        $result .= &dump_make($$node{'inputs'}{$input});
+      }
+    }
+    return $result;
+  }
+  print 'digraph {' . $nl;
+  print '  graph [ rankdir = LR, dir = back, nodesep = 0.03 ];' . $nl;
+  print '  node  [ shape = rect, style = rounded, height = 0 ];' . $nl;
+  print &dump_dot($root);
+  print '}' . $nl;
 
-my $lib_asts = [];
-foreach my $lib (@$libs) {
-  push @$lib_asts, &ast_path_from_so_path($lib);
+  print STDERR &dump_make($root);
+  #print STDERR &Dumper($root);
 }
-my $src_asts = [];
-foreach my $src (@$srcs) {
-  push @$src_asts, &ast_path_from_dk_path($src);
-}
-my $root = {};
-
-my $target_h_node = &add_node($root,
-                              &target_path('target.h'),
-                              [ &target_path('inputs.ast') ],
-                              [ '<cmd>', '--output', &target_path('target.h'), &target_path('inputs.ast') ],
-                            );
-my $inputs_ast_node = &add_node($target_h_node,
-                                &target_path('inputs.ast'),
-                                [ &target_path('srcs.ast'), @$lib_asts ],
-                                [ '<cmd>', '--output', &target_path('inputs.ast'), &target_path('srcs.ast'), @$lib_asts ],
-                              );
-foreach my $lib (@$libs) {
-  my $lib_ast = &ast_path_from_so_path($lib);
-  &add_node($inputs_ast_node,
-            $lib_ast,
-            [ $lib ],
-            [ '<cmd>', '--output', &target_path($lib_ast), $lib ],
-          );
-}
-my $srcs_ast_node = &add_node($inputs_ast_node,
-                              &target_path('srcs.ast'),
-                              [ @$src_asts ],
-                              [ '<cmd>', '--output', &target_path('srcs.ast'), @$src_asts ],
-                            );
-foreach my $src (@$srcs) {
-  my $src_ast = &ast_path_from_dk_path($src);
-  &add_node($srcs_ast_node,
-            $src_ast,
-            [ $src ],
-            [ '<cmd>', '--output', &target_path($src_ast), $src ],
-          );
-}
-print STDERR &Dumper($root);
-
-print 'digraph {' . $nl;
-print '  graph [ rankdir = LR, dir = back ];' . $nl;
-print '  node  [ shape = rect, style = rounded ];' . $nl;
-&dump_dot($root);
-print '}' . $nl;
