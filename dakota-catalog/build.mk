4df4210c783f528c02d53562d562edb5d0cfc41c
Robert Nielsen <robert@dakota.org>
2017-10-05 12:19:03 -0700

    Initial commit of dakota-dso/build.mk and dakota-catalog/build.mk.

---
+++ dakota-catalog/build.mk
@@ -0,0 +1,17 @@
+# -*- mode: makefile -*-
+
+$(shell mkdir -p /Users/robert/dakota/zzz/build/dakota-catalog)
+
+cxx :=    /usr/bin/clang++
+prefix := /Users/robert/dakota
+
+.PHONY : all dakota-catalog
+
+all : dakota-catalog
+dakota-catalog : /Users/robert/dakota/bin/dakota-catalog
+
+/Users/robert/dakota/bin/dakota-catalog : /Users/robert/dakota/zzz/build/dakota-catalog/dakota-catalog.cc.o
+	${cxx} @${prefix}/lib/dakota/linker.opts -Wl,-rpath,${prefix}/lib -o $@ $^ ${prefix}/lib/libdakota-dso.dylib
+
+/Users/robert/dakota/zzz/build/dakota-catalog/dakota-catalog.cc.o : /Users/robert/dakota/dakota-catalog/dakota-catalog.cc
+	${cxx} -c @${prefix}/lib/dakota/compiler.opts -I${prefix}/include -o $@ $<
