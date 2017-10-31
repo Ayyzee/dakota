82cabb36bb28c39f4b02226e7f7a0ac4f33b216e
Robert Nielsen <robert@dakota.org>
2017-10-31 12:27:00 -0700

    Remove generated build.mk and build.dot.

--- build.mk
+++
@@ -1,9 +0,0 @@
-include dakota-dso/build.mk
-include dakota-catalog/build.mk
-include dakota-find-library/build.mk
-
-include dakota-core/build.mk
-include dakota/build.mk
-
-include tst1/build.mk
-include tst2/build.mk
